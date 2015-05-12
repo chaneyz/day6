aebaf83b46c2e9822a2f8fad6150fffc10d53d93
Zach Chaney <zachary.chaney@gmail.com>
2015-05-12 13:35:49 -0500

    added the choose your own adventure

---
+++ javascript-intro/main.js
@@ -0,0 +1,16 @@
+// console.log(5+5);
+
+
+
+// console.log(x-5);
+
+if (myAge < 21) {
+	console.log('You\'re not old enough to view this site');
+}
+
+else {
+	console.log('You\'re old enough to view this site');
+}
+
+var myAge = 25
+
